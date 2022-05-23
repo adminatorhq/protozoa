056674571e925e596971333f7b82c1008fa43fef
Ayobami Akingbade <akingbadefred@gmail.com>
2022-05-23 21:46:32 +0100

    Export all from toast service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { AuthService } from './auth';
 export { ProgressService } from './progress';
 export { StorageService } from './storage';
 export { ToastService } from './toast';
+export { getBestErrorMessage } from './toast/utils';
