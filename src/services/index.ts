f1523fc070f33e5e1634edb0ce8c7a49875c6c35
Ayobami Akingbade <akingbadefred@gmail.com>
2022-05-23 23:46:30 +0100

    Add request service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,5 @@ export { AuthService } from './auth';
 export { ProgressService } from './progress';
 export { StorageService } from './storage';
 export { ToastService } from './toast';
+export { RequestService } from './request';
 export { getBestErrorMessage } from './toast/utils';
