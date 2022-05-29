e57aa83e2d52cc7698047a4ff2fea6a1a0a2f80d
Ayobami Akingbade <akingbadefred@gmail.com>
2022-05-30 00:41:37 +0100

    Escape dateformat

--- src/filters/time.filters.ts
+++ src/filters/time.filters.ts
@@ -1,18 +1,19 @@
-import dateformat from 'dateformat';
+// import dateformat from 'dateformat';
 import distanceInWordsToNow from 'date-fns/formatDistanceToNow';
 
 export class TimeFilters {
   static formatTime(time: string, type?: string): string {
-    if (!time) {
-      return 'N/A';
-    }
-    if (type === 'L') {
-      return dateformat(time, 'dS mmm yyyy, h:MM TT');
-    }
-    if (type === 'G') {
-      return dateformat(time, 'dS mmm yyyy');
-    }
-    return dateformat(time, 'mmmm dS, yyyy');
+    return `${time} ${type}`;
+    // if (!time) {
+    //   return 'N/A';
+    // }
+    // if (type === 'L') {
+    //   return dateformat(time, 'dS mmm yyyy, h:MM TT');
+    // }
+    // if (type === 'G') {
+    //   return dateformat(time, 'dS mmm yyyy');
+    // }
+    // return dateformat(time, 'mmmm dS, yyyy');
   }
 
   static timeAgo(time: Date): string {
