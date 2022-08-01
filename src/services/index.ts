139abd5b64bc5ad2ccc01cf7bd6f4a989e542bd8
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-01 17:09:57 +0100

    Export App storage

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { AuthService } from "./auth";
 export { StorageService } from "./storage";
+export { AppStorage } from "./storage/app";
 export { ToastService } from "./toast";
 export { getBestErrorMessage } from "./toast/utils";
